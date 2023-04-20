3bc052202663bb081e23d14d6ac65ff2c76511f2
jym-pa <jym36@ntrs.com>
2023-04-20 13:46:04 +0000

    back to original case sensitive logic

--- walkingtree/patternmatch_notthesetho.py
+++ walkingtree/patternmatch_notthesetho.py
@@ -13,15 +13,10 @@ def find(pattern, path):
     """search through filesystem based on given path location"""
     result = []
     for root, dirs, files in os.walk(path, topdown=True):
-        print("Here are the files found:")
-        print(files)
-        print("Here is the lowercase version of those files:")
-        lower_files = [x.lower() for x in files]
-        print(lower_files)
         if root in EXCLUDE: # if the root matches the exclude list
             dirs[:] = [] # remove the directory list for this iteration
             files[:] = [] # remove the file list for this iteration
-        for name in lower_files: # always perform the nested loop, but it maybe empty
+        for name in files: # always perform the nested loop, but it maybe empty
             if fnmatch.fnmatch(name, pattern): # if match
                 result.append(os.path.join(root, name)) # add to our list
     return result # return the list
@@ -31,13 +26,10 @@ def main():
     lookfor = input("What pattern am I looking for (Example: *.txt or *.cfg) ")
     print("Here is the pattern entered:")
     print(lookfor)
-    print("Here is the case insensitive (lowered) version:")
-    lower_lookfor = lookfor.lower()
-    print(lower_lookfor)
 
     lookwhere = input("What is the path in which I should search? ")
 
-    print("Results: ", find(lower_lookfor, lookwhere)) # call function
+    print("Results: ", find(lookfor, lookwhere)) # call function
 
 main()
 
