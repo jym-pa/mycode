ca3c655778f22acb916dd76c6a94e5f322f05e79
jym-pa <jym36@ntrs.com>
2023-04-21 16:47:42 +0000

    using pylint

---
+++ lesson53/smallscript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Mack | Mackoy Enterprises
+
+Program to test pylint
+"""
+
+def main():
+    """Our main program"""
+    mynote = "print this string" # define mynote var
+    print(mynote)                # display mynote as string to std_out
+
+main()
